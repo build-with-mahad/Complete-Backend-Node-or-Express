f34c8844e338e1d964b6646a2fc958942503fe75
Mahad Khan <mahadkhan122005@gmail.com>
2025-11-22 02:18:46 +0500

    Initialize Express application with middleware setup

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import cookieParser from "cookie-parser"
+import express from "express"
+const app = express()
+
+app.use(express.json({limit:"20kb"}))
+app.use(express.urlencoded({extended:true,limit:"20kb"}))
+app.use(express.static("./public"))
+app.use(cookieParser())
