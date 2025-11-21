5fd228363b6ba83f45f55250c09546e4d1eeb733
Mahad Khan <mahadkhan122005@gmail.com>
2025-11-22 02:13:37 +0500

    Fix MongoDB connection error logging format

--- src/db/db.js
+++ src/db/db.js
@@ -10,7 +10,7 @@ const connectDb = async () => {
         const connection = await mongoose.connect(`${process.env.MONGODB_URL}${DB_NAME}`)
         console.log(`Mongo DB Connected Successfully!.. ${connection.connection.host}`)
     } catch (error) {
-        console.log(`MongoDB not Connected..`, error)
+        console.log(`MongoDB not Connected..${error}`)
     }
 }
 export { connectDb }
@@ -21,7 +21,6 @@ export { connectDb }
 //     try {
 //         const connect = await mongoose.connect(`${process.env.MONGODB_URL}${DB_NAME}`)
 //         console.log(`Mondo Db connected Successfully !.. ${connect.connection.host}`)
-//         console.log(`Mongo Db connected successfully !..`)
 //         app.on('error', (error) => {
 //             console.log('ERROR', error)
 //         })
