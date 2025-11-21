03226f503b3e1974e03d52523cff7c7b90d51ee2
Mahad Khan <mahadkhan122005@gmail.com>
2025-11-22 01:55:45 +0500

    Add User model with authentication methods and environment configuration

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,10 @@
 import mongoose, { Schema } from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config({
+    path: './env'
+})
 const UserSchema = new Schema({
     username: {
         type: String,
@@ -41,4 +47,40 @@ const UserSchema = new Schema({
 }, {
     timestamps: true
 })
-export const User = mongoose.model("User",UserSchema)
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+})
+UserSchema.methods.isPassCorr = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+UserSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", UserSchema)
